29ca13194a134902d12fc0103970684f3a09a325
Chase <Chase.Kuhn1@oneamerica.com>
2022-07-14 13:54:33 -0400

    Updated base structure in main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
-provider "azurerm" {
-  features {}
-  subscription_id            = ""
-  tenant_id                  = ""
-  skip_provider_registration = true
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"
 }
 
-resource "azurerm_resource_group" "this" {
-  name     = "rg-00"
-  location = "eastus"
+provider "azurerm" {
+  features {}
+}
+
+# Resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-01"
+  location = "eastus2"
+  tags = {
+    env   = "dev"
+    owner = "chase"
+  }
 }
